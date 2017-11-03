428b501ae6fbce9a107d1221f3b459c94340af7a
kyraNET <kyradiscord@gmail.com>
2017-11-03 01:00:16 +0100

    Update to Hydrabolt's music rewrite

--- events/ready.js
+++ events/ready.js
@@ -3,7 +3,7 @@ const { Event } = require('klasa');
 module.exports = class extends Event {
 
     run() {
-        return this.client.user.setGame('m!help').catch(err => this.client.emit('log', err, 'error'));
+        return this.client.user.setActivity('m!help').catch(err => this.client.emit('log', err, 'error'));
     }
 
 };
