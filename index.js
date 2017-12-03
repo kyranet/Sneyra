f03015810fb5721a04b9d29a02c5a2b321467e88
kyraNET <kyradiscord@gmail.com>
2017-12-03 03:27:14 +0100

    Cleanup

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
-const Client = require('./lib/Client');
+const klasa = require('klasa');
+const Music = require('./lib/Music');
 const config = require('./config.json');
 
-const Sneyra = new Client({
-    prefix: 'm!',
-    cmdEditing: true,
-    disabledEvents: [
-        'GUILD_BAN_ADD',
-        'GUILD_BAN_REMOVE',
-        'TYPING_START',
-        'RELATIONSHIP_ADD',
-        'RELATIONSHIP_REMOVE',
-        'CHANNEL_PINS_UPDATE',
-        'PRESENCE_UPDATE',
-        'USER_UPDATE',
-        'USER_NOTE_UPDATE',
-        'MESSAGE_REACTION_ADD',
-        'MESSAGE_REACTION_REMOVE',
-        'MESSAGE_REACTION_REMOVE_ALL'
-    ],
-    console: { useColor: true }
-});
+klasa.Client.defaultPermissionLevels
+    .addLevel(5, false, (client, msg) => msg.guild && msg.guild.configs.dj && msg.member.roles.has(msg.guild.configs.dj))
+    .addLevel(6, false, (client, msg) => msg.guild &&
+        ((msg.guild.configs.administrator && msg.member.roles.has(msg.guild.configs.administrator)) ||
+            msg.member.permissions.has('MANAGE_GUILD')));
+
+class Sneyra extends klasa.Client {
+
+    constructor() {
+        super({
+            prefix: 'm!',
+            cmdEditing: true,
+            disabledEvents: [
+                'GUILD_BAN_ADD',
+                'GUILD_BAN_REMOVE',
+                'TYPING_START',
+                'RELATIONSHIP_ADD',
+                'RELATIONSHIP_REMOVE',
+                'CHANNEL_PINS_UPDATE',
+                'PRESENCE_UPDATE',
+                'USER_UPDATE',
+                'USER_NOTE_UPDATE',
+                'MESSAGE_REACTION_ADD',
+                'MESSAGE_REACTION_REMOVE',
+                'MESSAGE_REACTION_REMOVE_ALL'
+            ],
+            console: { useColor: true }
+        });
+
+        this.queue = new Music();
+    }
+
+}
 
 Sneyra.login(config.token);
-Sneyra.once('ready', () => {
-    for (const guild of Sneyra.guilds.values()) Sneyra.queue.create(guild);
-});
