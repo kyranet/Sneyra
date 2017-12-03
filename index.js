38869c74c65d116ec8e1c8febda044f1dc8779e5
kyraNET <kyradiscord@gmail.com>
2017-12-03 03:44:00 +0100

    Fix disabledEvents not being passed correctly

--- index.js
+++ index.js
@@ -12,23 +12,25 @@ class Sneyra extends klasa.Client {
 
     constructor() {
         super({
-            prefix: 'm!',
+            clientOptions: {
+                disabledEvents: [
+                    'GUILD_BAN_ADD',
+                    'GUILD_BAN_REMOVE',
+                    'TYPING_START',
+                    'RELATIONSHIP_ADD',
+                    'RELATIONSHIP_REMOVE',
+                    'CHANNEL_PINS_UPDATE',
+                    'PRESENCE_UPDATE',
+                    'USER_UPDATE',
+                    'USER_NOTE_UPDATE',
+                    'MESSAGE_REACTION_ADD',
+                    'MESSAGE_REACTION_REMOVE',
+                    'MESSAGE_REACTION_REMOVE_ALL'
+                ]
+            },
+            console: { useColor: true },
             cmdEditing: true,
-            disabledEvents: [
-                'GUILD_BAN_ADD',
-                'GUILD_BAN_REMOVE',
-                'TYPING_START',
-                'RELATIONSHIP_ADD',
-                'RELATIONSHIP_REMOVE',
-                'CHANNEL_PINS_UPDATE',
-                'PRESENCE_UPDATE',
-                'USER_UPDATE',
-                'USER_NOTE_UPDATE',
-                'MESSAGE_REACTION_ADD',
-                'MESSAGE_REACTION_REMOVE',
-                'MESSAGE_REACTION_REMOVE_ALL'
-            ],
-            console: { useColor: true }
+            prefix: 'm!'
         });
 
         this.queue = new Music();
